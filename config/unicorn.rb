b9e7248c17b386cf647c9fc2cd8b2fe7c2529485
Dai Takahashi <baddestbeat@yahoo.co.jp>
2016-03-21 22:48:17 +0900

    found my old unicorn settings

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,37 +1,47 @@
-rails_root = File.expand_path('../../', __FILE__)
-# rails_env = ENV['RAILS_ENV'] || "development"
-
-ENV['BUNDLE_GEMFILE'] = rails_root + "/Gemfile"
-
+@app_path = '/var/www/api'
 worker_processes 2
+working_directory "#{@app_path}"
 
-working_directory rails_root
 
-timeout 30
-
-stderr_path File.expand_path('../../log/unicorn_stderr.log', __FILE__)
-stdout_path File.expand_path('../../log/unicorn_stdout.log', __FILE__)
-
-listen File.expand_path('../../tmp//unicorn.sock', __FILE__)
-listen 8080
-
-pid File.expand_path('../../tmp/pids/unicorn.pid', __FILE__)
+# This loads the application in the master process before forking
+# worker processes
+# Read more about it here:
+# http://unicorn.bogomips.org/Unicorn/Configurator.html
 
 preload_app true
+timeout 30
 
+# This is where we specify the socket.
+# We will point the upstream Nginx module to this socket later on
+listen "/var/www/api/tmp/unicorn.sock", :backlog => 64
+pid "/var/www/api/tmp/unicorn.pid"
+
+# Set the path of the log files inside the log folder of the testapp
+stderr_path "/var/log/unicorn/unicorn.stderr.log"
+stdout_path "/var/log/unicorn/unicorn.stdout.log"
+
+
+
+#before_fork=>masterがworkerを産む直前に実行する処理を登録するメソッド
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.connection.disconnect!
+  # 確率されたコネクションを切断してそれぞれのプロセスで新たにサーバに接続(after_fork_do)
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 
+
+  # workerの数が1以上ならTTOUを送ってworkerを減らす
+  # workerの数が1なら古いmasterをkillする
   old_pid = "#{server.config[:pid]}.oldbin"
-  if old_pid != server.pid
-    begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
+    if old_pid != server.pid
+      begin
+        sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+        Process.kill(sig, File.read(old_pid).to_i)
+      rescue Errno::ENOENT, Errno::ESRCH
+      end
     end
+
+    # 順次workerをkillするために、forkされる速度を落とす
+    sleep 1
   end
-end
 
 after_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
